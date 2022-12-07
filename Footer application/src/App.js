a1edd938b1170508d6609c8775c5c5078c7fc731
Akash Bhandwalkar <akash.bhandwalkar@clairvoyantsoft.com>
2022-12-07 14:43:26 +0530

    moved footer app

---
+++ Footer application/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <footer className="App-footer">
+        <span>&copy; 2021 Meetup demo - react footer. All rights reserved.</span>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
